075696e879818ef10afb3c681b93be54a64830bd
tsjuusmei <guusmay2@hotmail.com>
2020-03-13 10:11:22 +0100

    db working with simpler code

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const find = require('array-find')
 const slug = require('slug')
 const multer = require('multer')
 const mongo = require('mongodb')
-const {MongoClient} = require('mongodb')
+// const {MongoClient} = require('mongodb')
 require('dotenv').config()
 
 const app = express()
@@ -17,30 +17,13 @@ const upload = multer({dest: 'static/upload'})
 let db = null
 const uri = process.env.DB_URI
 
-async function findDb(collection, searchValue){
-    console.log('findindb func')
-
-    // Source: https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database
-    
-    const uri = process.env.DB_URI
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
-
-        const db = client.db('db');
-
-        const informatie = await db.collection(collection).find(searchValue).toArray();
-        console.log(informatie)
-		return informatie
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
+mongo.MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
+    if (err) {
+        throw err
     }
-}
+
+    db = client.db(process.env.DB_NAME)
+})
 
 // static files from static folders
 app.use('/static', express.static('static'))
@@ -54,9 +37,20 @@ app.get('/', (req, res) => res.render('profile'))
 app.get('/likes', (req, res) => res.render('likes'))
 app.get('/visitors', (req, res) => res.render('visitors'))
 app.get('/profile', (req, res) => res.render('profile'))
+app.get('/test', test)
 
-// calling function for searching database
-findDb('fakeUsers')
+function test(req, res) {
+    db.collection('fakeUsers').find().toArray(done)
+
+    function done(err, data) {
+        if (err) {
+            next(err);
+        } else {
+            console.log(data);
+            res.render('test.ejs', {data: data});
+        }
+    }
+}
   
 // 404 when not found
 app.use((req, res) => res.status(404).send('404'))
