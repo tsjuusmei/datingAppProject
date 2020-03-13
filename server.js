70ac46bffacc7a82299b17a7943707e53ca3ca7b
tsjuusmei <guusmay2@hotmail.com>
2020-03-13 09:28:04 +0100

    removed unused code for excersizes, working db but not with simple code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const find = require('array-find')
 const slug = require('slug')
 const multer = require('multer')
 const mongo = require('mongodb')
+const {MongoClient} = require('mongodb')
 require('dotenv').config()
 
 const app = express()
@@ -16,35 +17,30 @@ const upload = multer({dest: 'static/upload'})
 let db = null
 const uri = process.env.DB_URI
 
-mongo.MongoClient.connect(uri, (err, client) => {
-    if (err) { throw err } 
-    db = client.db(process.env.DB_NAME)
-})
+async function findDb(collection, searchValue){
+    console.log('findindb func')
 
-// async function findDb(collection, searchValue){
-//     console.log('findindb func')
-
-//     // Source: https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database
+    // Source: https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database
     
-//     const uri = process.env.DB_URI
-//     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const uri = process.env.DB_URI
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//     try {
-//         // Connect to the MongoDB cluster
-//         await client.connect();
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
 
-//         const db = client.db('db');
+        const db = client.db('db');
 
-//         const informatie = await db.collection(collection).find(searchValue).toArray();
-//         console.log(informatie)
-// 		return informatie
+        const informatie = await db.collection(collection).find(searchValue).toArray();
+        console.log(informatie)
+		return informatie
  
-//     } catch (e) {
-//         console.error(e);
-//     } finally {
-//         await client.close();
-//     }
-
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
 
 // static files from static folders
 app.use('/static', express.static('static'))
@@ -58,86 +54,9 @@ app.get('/', (req, res) => res.render('profile'))
 app.get('/likes', (req, res) => res.render('likes'))
 app.get('/visitors', (req, res) => res.render('visitors'))
 app.get('/profile', (req, res) => res.render('profile'))
-app.get('/test', test)
-
-
-
-// practices for Backend class
-
-// app.get('/add', (req, res) => res.render('add'))
-// app.get('/:id', detail)
-
-// post route for form
-// app.post('/', upload.single('cover'), add)
-
-// function detail(req, res, next) {
-//     const id = req.params.id
-//     const movie = find(data, function(movie) {
-//         return movie.id === id
-//     })
-
-//     if(!movie) {
-//         next()
-//         return
-//     }
-
-//     res.render('detail.ejs', {data: movie})
-// }
-
-// let data = [
-//     {
-//         id: 'film1',
-//         title: 'filmeen',
-//         plot: 'plot voor film een',
-//         description: 'de descriptie voor film een gaat als voort:'
-//     },
-//     {
-//         id: 'film2',
-//         title: 'filmtwee',
-//         plot: 'plot voor film twee',
-//         description: 'de descriptie voor film twee is heel anders'
-//     }
-// ]
-
-// function add(req, res) {
-//     const id = slug(req.body.title).toLowerCase()
-  
-//     data.push({
-//       id: id,
-//       title: req.body.title,
-//       cover: req.file ? req.file.filename : null,
-//       plot: req.body.plot,
-//       description: req.body.description
-//     })
-  
-//     res.redirect('/' + id)
-//   }
-
-//   function remove(req, res) {
-//     var id = req.params.id
-  
-//     data = data.filter(function (value) {
-//       return value.id !== id
-//     })
-  
-//     res.json({status: 'ok'})
-//   }
 
 // calling function for searching database
-// findDb('fakeUsers')
-
-function test(req, res) {
-    db.collection('fakeUsers').find().toArray(done)
-
-    function done(err, data) {
-        if(err) {
-            next(err)
-        } else {
-            console.log(data)
-            res.render('test.ejs', {data: data})
-        }
-    }
-}
+findDb('fakeUsers')
   
 // 404 when not found
 app.use((req, res) => res.status(404).send('404'))
