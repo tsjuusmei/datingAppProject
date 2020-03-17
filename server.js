61ea4064dc8e6ce0801e193b4bffe1b0e0a59450
tsjuusmei <guusmay2@hotmail.com>
2020-03-17 14:50:09 +0100

    added routing with dynamic data from database and functions for this

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const find = require('array-find')
 const slug = require('slug')
 const multer = require('multer')
 const mongo = require('mongodb')
+const ObjectID  = mongo.ObjectID;
 
 const app = express()
 
@@ -26,6 +27,16 @@ mongo.MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true
     db = client.db(process.env.DB_NAME)
 })
 
+// session for user
+app.use(session({ 
+    resave: false,
+    saveUninitialized: true,
+    secure: true,
+    secret: process.env.SESSION_SECRET
+}));
+
+// source used https://flaviocopes.com/express-sessions/ and https://www.npmjs.com/package/express-session
+
 // static files from static folders
 app.use('/static', express.static('static'))
 
@@ -34,15 +45,38 @@ app.set('view engine', 'ejs')
 
 
 // routing with ejs files
-app.get('/', (req, res) => res.render('profile'))
-app.get('/likes', (req, res) => res.render('likes'))
-app.get('/visitors', (req, res) => res.render('visitors'))
+app.get('/', (req, res) => {
+    req.session.user = '5e6ba1bde4b2e66c746a4df7';
+    res.render('profile')
+})
+app.get('/likes', async (req, res) => {
+    try {
+        const user = await db.collection('fakeUsers').findOne({ _id: ObjectID(req.session.user)})   // find logged in user by ID
+        const promises = [];    // create an array to store promises
+        user.likedBy.forEach((likerId) => { // make a foreach for each visitor in visitedBy array in db
+            promises.push(db.collection('fakeUsers').findOne({ _id: new ObjectID(likerId)})) // For each visitor in the visitedBy array, get the corresponding user from the database and push it as a promise to the promises[]
+        })
+        const likes = await Promise.all(promises) // wait for all promises to finish
+        res.render('likes', {likes: likes}) // render likes.ejs with likers data
+    } catch(err) {
+        console.log(err) // log error if there is an error
+    }
+  })
+app.get('/visitors', async (req, res) => {
+    try {
+        const user = await db.collection('fakeUsers').findOne({ _id: ObjectID(req.session.user)})   // find logged in user by ID
+        const promises = [];    // create an array to store promises
+        user.visitedBy.forEach((visitorId) => { // make a foreach for each visitor in visitedBy array in db
+            promises.push(db.collection('fakeUsers').findOne({ _id: new ObjectID(visitorId)})) // For each visitor in the visitedBy array, get the corresponding user from the database and push it as a promise to the promises[]
+        })
+        const visitors = await Promise.all(promises) // wait for all promises to finish
+        res.render('visitors', {visitors: visitors}) // render visitors.ejs with visitors data
+    } catch(err) {
+        console.log(err) // log error if there is an error
+    }
+  })
 app.get('/profile', (req, res) => res.render('profile'))
 app.get('/test', test)
-
-function visitors(req, res) {
-    db.collection('fakeUsers').find().
-}
 
 function test(req, res) {
     db.collection('fakeUsers').find().toArray(done)
@@ -59,14 +93,6 @@ function test(req, res) {
   
 // 404 when not found
 app.use((req, res) => res.status(404).send('404'))
-
-// session for user
-app.use(session({ 
-    resave: false,
-    saveUninitialized: true,
-    secure: true,
-    secret: process.env.SESSION_SECRET
-}));
 
 // listen to port and display the port in console
 app.listen(port, () => console.log('listening on port ' + port));
