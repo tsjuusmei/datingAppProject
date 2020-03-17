255b49abffc49a48dba28ef55dc3e29b708f9ed9
tsjuusmei <guusmay2@hotmail.com>
2020-03-17 15:24:38 +0100

    added dynamic data for likes and profile, with the functions in the routing

--- server.js
+++ server.js
@@ -45,19 +45,24 @@ app.set('view engine', 'ejs')
 
 
 // routing with ejs files
-app.get('/', (req, res) => {
-    req.session.user = '5e6ba1bde4b2e66c746a4df7';
-    res.render('profile')
+app.get('/', async (req, res) => {
+    try {
+        req.session.user = '5e6ba1bde4b2e66c746a4df7';
+        const user = await db.collection('fakeUsers').findOne({ _id: ObjectID(req.session.user)})   // find logged in user by ID
+        res.render('profile', {user})
+    } catch (err) {
+        console.log(err)
+    }
 })
 app.get('/likes', async (req, res) => {
     try {
         const user = await db.collection('fakeUsers').findOne({ _id: ObjectID(req.session.user)})   // find logged in user by ID
         const promises = [];    // create an array to store promises
-        user.likedBy.forEach((likerId) => { // make a foreach for each visitor in visitedBy array in db
-            promises.push(db.collection('fakeUsers').findOne({ _id: new ObjectID(likerId)})) // For each visitor in the visitedBy array, get the corresponding user from the database and push it as a promise to the promises[]
+        user.likedBy.forEach((likerId) => { // make a foreach for each like in likedBy array in db
+            promises.push(db.collection('fakeUsers').findOne({ _id: new ObjectID(likerId)})) // For each like in the likedBy array, get the corresponding user from the database and push it as a promise to the promises[]
         })
         const likes = await Promise.all(promises) // wait for all promises to finish
-        res.render('likes', {likes: likes}) // render likes.ejs with likers data
+        res.render('likes', {likes: likes, user}) // render likes.ejs with likers data
     } catch(err) {
         console.log(err) // log error if there is an error
     }
@@ -70,12 +75,19 @@ app.get('/visitors', async (req, res) => {
             promises.push(db.collection('fakeUsers').findOne({ _id: new ObjectID(visitorId)})) // For each visitor in the visitedBy array, get the corresponding user from the database and push it as a promise to the promises[]
         })
         const visitors = await Promise.all(promises) // wait for all promises to finish
-        res.render('visitors', {visitors: visitors}) // render visitors.ejs with visitors data
+        res.render('visitors', {visitors: visitors, user}) // render visitors.ejs with visitors data
     } catch(err) {
         console.log(err) // log error if there is an error
     }
   })
-app.get('/profile', (req, res) => res.render('profile'))
+app.get('/profile', async (req, res) => {
+    try {
+        const user = await db.collection('fakeUsers').findOne({ _id: ObjectID(req.session.user)})   // find logged in user by ID
+        res.render('profile', {user});
+    } catch(err) {
+        console.log(err)
+    }
+})
 app.get('/test', test)
 
 function test(req, res) {
